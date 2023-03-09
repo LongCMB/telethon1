737073b98808e04bc917254649762ce27c74f22b
RENAUD-DEV\Renaud <rereber16@gmail.com>
2023-03-09 16:00:40 +0100

    debut formulaire

---
+++ form.php
@@ -0,0 +1,22 @@
+        <div class="container d-flex justify-content-center h-100">
+            <div class="row col-md-10 bg-danger">
+                <label for="name_item" class="col-md-10">"Gestion des trucs"</label>
+                <div class="col-md-10">
+                    <label for="name_action"> "Ajout de machin" </label>
+                    <form action="" method="post">
+                        <div class="lb_inp 1 align-items-center">
+                            <label for="desc"> "Nom" </label><br>
+                            <input type="text" name="desc" id="desc">
+                        </div>
+                        <div class="lb_inp 2">
+                            <label for="desc"> "Prenom" </label><br>
+                            <input type="text" name="desc" id="desc">
+                        </div>
+                        <div class="lb_inp 3">
+                            <label for="desc"> "Date de naissance" </label><br>
+                            <input type="text" name="desc" id="desc">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
